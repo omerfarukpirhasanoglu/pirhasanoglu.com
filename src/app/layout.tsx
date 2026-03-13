c3e3f2db83e4310aad52496dc6e2b5fc5ae6f224
omerfarukpirhasanoglu <omerpirhasanoglu@gmail.com>
2026-03-14 02:37:48 +0300

    noise fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <div 
           className="fixed inset-0 z-50 pointer-events-none"
           style={{
-            opacity: "0.25", // Çok yüksek yapma, göz yorar
+            opacity: "0.10", // Çok yüksek yapma, göz yorar
             mixBlendMode: "screen", // Siyahın üzerinde 'parlamasını' sağlar
             backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3CfeColorMatrix type='saturate' values='0'/%3E%3CfeComponentTransfer%3E%3CfeFuncR type='discrete' tableValues='0 1'/%3E%3CfeFuncG type='discrete' tableValues='0 1'/%3E%3CfeFuncB type='discrete' tableValues='0 1'/%3E%3C/feComponentTransfer%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
           }}
