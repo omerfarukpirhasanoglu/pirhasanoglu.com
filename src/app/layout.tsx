e7371a383a74ffbd42d8cc5c1fcd47fe4e2542f7
omerfarukpirhasanoglu <omerpirhasanoglu@gmail.com>
2026-03-14 02:32:41 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,16 @@ export default function RootLayout({
       {/*relative*/}
       <body className={`${inter.className} bg-background text-gray-100 antialiased min-h-screen flex flex-col relative`}>
         
-        {/*Noise*/}
+        {/* Noise Overlay */}
         <div 
-          className="fixed inset-0 z-50 pointer-events-none opacity-[0.65]"
+          className="fixed inset-0 z-50 pointer-events-none"
           style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' 
-            baseFrequency='0.75' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+            opacity: "0.4",
+            mixBlendMode: "overlay", // Arka planla kaynaşması için
+            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter 
+            id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.60' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect 
+            width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+            filter: "contrast(150%) brightness(100%)", // Dokuyu keskinleştirme
           }}
         />
 
