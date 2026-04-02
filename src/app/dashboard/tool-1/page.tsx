659157df6c8acc302dac9711123ca26489568fda
omerfarukpirhasanoglu <omerpirhasanoglu@gmail.com>
2026-04-03 00:46:44 +0300

    tool1 backgorund added

--- src/app/dashboard/tool-1/page.tsx
+++ src/app/dashboard/tool-1/page.tsx
@@ -1,13 +1,28 @@
 import ImageAnalyzer from "@/src/components/features/ImageAnalyzer";
+import Image from "next/image";
 
 export default function Tool1Page() {
   return (
-    <div className="w-full animate-in fade-in slide-in-from-bottom-4 duration-500">
-      <ImageAnalyzer
-        title="Chroma"
-        titleBadge="v1.2"
-        description="Kıyafetinizi yapay zeka ile görün. Stil, renk uyumu ve moda puanı hakkında anında geri bildirim alın."
-      />
+    <div className="relative w-full min-h-screen pb-20">
+
+      <div className="fixed inset-0 z-0 overflow-hidden pointer-events-none">
+        <Image
+          src="/dashboardback.png"
+          alt="Background"
+          fill
+          className="object-cover object-center opacity-30"
+          priority
+        />
+      </div>
+
+      <div className="relative z-10 w-full animate-in fade-in slide-in-from-bottom-4 duration-500">
+        <ImageAnalyzer
+          title="Chroma"
+          titleBadge="v1.2"
+          description="Kıyafetinizi yapay zeka ile görün. Stil, renk uyumu ve moda puanı hakkında anında geri bildirim alın."
+        />
+      </div>
+
     </div>
   );
 }
