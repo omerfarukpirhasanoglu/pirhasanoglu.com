b9bde89eef455d66871010c75070d0bf9cd4f86a
omer pirhasanoglu <omerpirhasanoglu@gmail.com>
2026-06-07 21:52:26 +0300

    sitemap added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,25 +5,25 @@ export const dynamic = 'force-static'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://siteadresi.com',
+      url: 'https://pirhasanoglu.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://siteadresi.com/dashboard',
+      url: 'https://pirhasanoglu.com/dashboard',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://siteadresi.com/dashboard/tool-1',
+      url: 'https://pirhasanoglu.com/dashboard/tool-1',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-      url: 'https://siteadresi.com/dashboard/tool-2',
+      url: 'https://pirhasanoglu.com/dashboard/tool-2',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
